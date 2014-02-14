2a667aa77726727ea0aca4ebaa4edf364a1c8f6c
Joshua Hoblitt <jhoblitt@cpan.org>
2014-02-14 00:43:54 -0700

    add rspec coverage of service enable on Debian

--- spec/classes/sysstat_service_spec.rb
+++ spec/classes/sysstat_service_spec.rb
@@ -1,14 +1,35 @@
 require 'spec_helper'
 
 describe 'sysstat::service', :type => :class do
+  context 'on RedHat' do
+    it do
+      should contain_service('sysstat').with({
+        :hasstatus  => 'false',
+        :hasrestart => 'true',
+        :enable     => 'true',
+      })
+    end
 
-  it do
-    should contain_service('sysstat').with({
-      :hasstatus  => 'false',
-      :hasrestart => 'true',
-      :enable     => 'true',
-    })
-  end
+    it { should_not contain_augeas('/etc/default/sysstat') }
+  end # on RedHat
 
+  context 'on Debian' do
+    let(:facts) {{ :osfamily => 'Debian' }}
+
+    it do
+      should contain_service('sysstat').with({
+        :hasstatus  => 'false',
+        :hasrestart => 'true',
+        :enable     => 'true',
+      })
+    end
+
+    it do
+      should contain_augeas('/etc/default/sysstat').with({
+        :context => '/files/etc/default/sysstat/',
+        :changes => 'set ENABLED true',
+      })
+    end
+  end # on Debian
 end
 
